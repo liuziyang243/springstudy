cffa910f300ba382ccbe0d9e72d3a47c9170db00
lzy <liuziyang@crscd.com.cn>
2017-11-29 16:33:56 +0800

    初始化项目

---
+++ src/main/java/com/crscd/springtest/app/App.java
@@ -0,0 +1,13 @@
+package com.crscd.springtest.app;
+
+/**
+ * @author lzy
+ * <p>
+ * Create Time: 2017/11/29 16:25
+ * @version v1.00
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
